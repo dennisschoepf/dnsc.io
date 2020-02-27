3dbc1991a107dce0759e75f574e2f896a01fd65d
Dennis <dennis@schoepf.co>
2020-02-27 15:34:04 +0100

    Adapt links for accessibility, add meta tags and lang

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ export default () => (
   <div className="container mx-auto my-2 md:my-4 flex flex-row justify-between font-bold text-lg md:text-xl tracking-wide uppercase">
     <Link href="/"><a className="py-2 px-4">Dennis Schoepf</a></Link>
     <Link href="/about">
-      <a className="py-2 px-4 hover:bg-teal-500 hover:text-white hover:shadow-lg rounded">About</a>
+      <a className="py-2 px-4 hover:bg-teal-600 hover:text-white hover:shadow-lg rounded">About</a>
     </Link>
   </div>
 )
--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <main>
       <Head>
+        <title>schoepf.co - Intro & Projects</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Hello there, I am Dennis Schoepf a designer/developer with over 5 years of professional experience, currently doing a master's degree in Human-Computer-Interaction in Salzburg, Austria."/>
+        <meta name="keywords" content="Dennis Schoepf, Dennis, Schoepf, Personal Website, Projects" />
+        <meta name="author" content="Dennis Schoepf" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="/public/favicon.ico" />
       </Head>
       <Nav />
