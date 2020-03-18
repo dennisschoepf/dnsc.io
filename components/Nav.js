b297f2f1a2dfd4ed86e0af03a825dd13089e7395
Dennis Schoepf <dennis@schoepf.co>
2020-03-18 16:03:58 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default () => (
   <div className="container mx-auto my-2 md:my-4 flex flex-row justify-between font-bold text-lg md:text-xl tracking-wide uppercase">
-    <Link href="/"><a className="py-2 px-4">dnsc.io</a></Link>
+    <Link href="/"><a className="py-2 px-4">dnsc</a></Link>
     <Link href="/about">
       <a className="nav-link-hover-none py-2 px-4 hover:bg-teal-600 hover:text-white hover:shadow-lg rounded">About</a>
     </Link>
