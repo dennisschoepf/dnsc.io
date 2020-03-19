744be8dcfc19c6459a9d4075ece28d89b8abf3cf
Dennis <dennis@schoepf.co>
2020-03-19 17:38:34 +0100

    Add favicons and update packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { MDXProvider } from '@mdx-js/react';
-import '../styles/index.css';
-import Head from 'next/head';
-import Headline from '../components/Headline';
-import Paragraph from '../components/Paragraph';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
+import { MDXProvider } from "@mdx-js/react";
+import "../styles/index.css";
+import Head from "next/head";
+import Headline from "../components/Headline";
+import Paragraph from "../components/Paragraph";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const mdxComponents = {
   h1: Headline,
@@ -16,12 +16,21 @@ function MyApp({ Component, pageProps }) {
     <MDXProvider components={mdxComponents}>
       <main>
         <Head>
-          <title>schoepf.co - Intro & Projects</title>
+          <title>dnsc.io - Dennis Schoepf</title>
           <meta charSet="UTF-8" />
-          <meta name="description" content="Hello there, I am Dennis Schoepf a designer/developer with over 5 years of professional experience, currently doing a master's degree in Human-Computer-Interaction in Salzburg, Austria."/>
-          <meta name="keywords" content="Dennis Schoepf, Dennis, Schoepf, Personal Website, Projects" />
+          <meta
+            name="description"
+            content="Hello there, I am Dennis Schoepf a designer/developer with over 5 years of professional experience, currently doing a master's degree in Human-Computer-Interaction in Salzburg, Austria."
+          />
+          <meta
+            name="keywords"
+            content="Dennis Schoepf, Dennis, Schoepf, Personal Website, Projects"
+          />
           <meta name="author" content="Dennis Schoepf" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <link rel="shortcut icon" href="/public/favicon.ico" />
         </Head>
         <Nav />
@@ -30,6 +39,6 @@ function MyApp({ Component, pageProps }) {
       </main>
     </MDXProvider>
   );
-};
+}
 
 export default MyApp;
