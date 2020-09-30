f78478d1d3df13bd063340dca33de19324f348f2
Dennis Schoepf <dennis@schoepf.co>
2020-10-01 01:34:48 +0200

    Fix favicon link tags

--- pages/_document.js
+++ pages/_document.js
@@ -32,9 +32,23 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
             rel="stylesheet"
