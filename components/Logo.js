ac58b5b9daa7e8923894af84d96dd96a392decba
Dennis Schoepf <dennis@schoepf.co>
2020-10-01 00:33:21 +0200

    Adapt logo size on larger screens

--- components/Logo.js
+++ components/Logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
+import media from 'styled-media-query';
 
 const LogoContainer = styled.div`
   background-color: ${(props) => props.theme.colors.white};
@@ -15,6 +16,10 @@ const LogoFont = styled.a`
   font-size: 2.8rem;
   color: ${(props) => props.theme.colors.black};
   transform: rotate(12deg);
+
+  ${media.greaterThan('medium')`
+    font-size: 3.2rem;
+  `}
 `;
 
 export default function Logo() {
