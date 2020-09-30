127913882a6d7cddbebeabc7143b90d306b38ebd
Dennis Schoepf <dennis@schoepf.co>
2020-10-01 00:12:17 +0200

    Remove Hero component

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,12 @@
 import { Fragment } from 'react';
 import styled, { useTheme } from 'styled-components';
-import Hero from '../components/Hero';
 import Title from '../components/Title';
-import Shape, { shapes } from '../components/Shape';
-import Container from '../components/Container';
-
-const BigFontContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 1rem 0 0 0;
-`;
-
-const BigFont = styled.p`
-  color: ${(props) => props.theme.colors.white};
-  font-size: ${(props) => props.theme.sizes.xl};
-  font-family: ${(props) => props.theme.font};
-  font-weight: 700;
-  padding: 0;
-  margin: 0 1rem 0 0;
-`;
 
 export default function Home() {
   const theme = useTheme();
 
   return (
     <Fragment>
-      <Hero>
-        <Title>Hi, I'm Dennis.</Title>
-        <BigFontContainer>
-          <BigFont>Student</BigFont>
-          <Shape
-            size={theme.sizes.m}
-            color={theme.colors.yellow}
-            shape={shapes.CIRCLE}
-          />
-        </BigFontContainer>
-        <BigFontContainer>
-          <BigFont>Developer</BigFont>
-          <Shape
-            size={theme.sizes.m}
-            color={theme.colors.blue}
-            shape={shapes.SQUARE}
-          />
-        </BigFontContainer>
-        <BigFontContainer>
-          <BigFont>Designer</BigFont>
-          <Shape
-            size={theme.sizes.m}
-            color={theme.colors.red}
-            shape={shapes.TRIANGLE}
-          />
-        </BigFontContainer>
-      </Hero>
       <div
         style={{
           height: '100%',
