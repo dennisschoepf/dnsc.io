aa42710bc2e10ded0993dd3c55777ec0bddae386
Dennis Schoepf <dennis@schoepf.co>
2020-10-29 12:57:16 +0100

    Implment mobile footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import media from 'styled-media-query';
 import Container from '../components/Container';
 import { useColors } from '../hooks/useTheme';
 import Button from './Button';
+
+const FooterFlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: ${(props) => props.theme.sizes.xxl} 0;
+`;
 
 const FooterContactHeadline = styled.h3`
   width: 50%;
@@ -11,21 +19,42 @@ const FooterContactHeadline = styled.h3`
   font-size: ${(props) => props.theme.sizes.xxl};
   line-height: 120%;
   color: ${(props) => props.theme.colors.white};
-`;
 
-const FooterFlexContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  ${media.lessThan('medium')`
+    margin-right: 0;
+    width: 100%;
+    text-align: center;
+  `}
+
+  ${media.lessThan('350px')`
+    font-size: ${(props) => props.theme.sizes.xl}
+  `}
 `;
 
 const FooterContactButton = styled.div`
   width: 50%;
+
+  ${media.lessThan('medium')`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: ${({ theme }) => theme.sizes.xxxl};
+  `}
 `;
 
 const FooterLegal = styled.small`
   width: 50%;
   color: ${(props) => props.theme.colors.grey};
   text-align: right;
+
+  ${media.lessThan('medium')`
+    width: 100%;
+    text-align: center;
+  `}
+
+  ${media.lessThan('350px')`
+    font-size: ${(props) => props.theme.sizes.m}
+  `}
 `;
 
 export default function Footer() {
