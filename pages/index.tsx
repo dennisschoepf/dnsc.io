1c96614ba3ee19b344a19c5ede0e6a18bbb0e768
Dennis Schoepf <me@dnsc.io>
2021-01-17 22:04:35 +0100

    Use lang hook instead of direct file access

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,38 @@ import { Fragment } from 'react';
 import Link from 'next/link';
 import Container, { ContainerType } from '../components/Container';
 import Icon from '../components/Icon';
-import { lang } from '../content/lang';
 import DocumentTitle from '../components/DocumentTitle';
+import { useLang } from '../hooks/useLang';
 
 export default function IndexPage() {
+  const { index } = useLang();
+
   return (
     <Fragment>
-      <DocumentTitle title={lang.index.documentTitle} />
+      <DocumentTitle title={index.documentTitle} />
       <main>
         <section id="index">
           <Container type={ContainerType.SMALL} className="pt-36">
             <h1 className="mb-8 text-5xl lg:text-7xl text-white font-bold">
-              <span className="text-gradient">{lang.index.title[0]}</span>{' '}
-              {lang.index.title[1]},
+              <span className="text-gradient">{index.title[0]}</span>{' '}
+              {index.title[1]},
             </h1>
             <p className="text-2xl text-white leading-normal">
-              {lang.index.introText[0]}{' '}
-              {lang.index.introLinks.map((introLink, i) => [
+              {index.introText[0]}{' '}
+              {index.introLinks.map((introLink, i) => [
                 <Link href={introLink.href} key={introLink.text}>
                   <a className="border-b-2 border-gray-500 hover:border-white">
                     {introLink.text}
                   </a>
                 </Link>,
-                i === lang.index.introLinks.length - 1 ? '.' : ', ',
+                i === index.introLinks.length - 1 ? '.' : ', ',
               ])}
             </p>
             <p className="text-2xl text-white leading-normal mt-8">
-              {lang.index.introText[1]}
+              {index.introText[1]}
             </p>
             <ul className="flex mt-2">
-              {lang.index.contactLinks.map((contactLink) => (
+              {index.contactLinks.map((contactLink) => (
                 <li className="mr-4" key={contactLink.href}>
                   <Link href={contactLink.href}>
                     <a target="_blank">
@@ -46,7 +48,7 @@ export default function IndexPage() {
         <section id="project-preview">
           <Container type={ContainerType.SMALL}>
             <h2 className="text-5xl lg:text-8xl uppercase tracking-wide text-right text-gray-800 font-bold mt-0 mb-4">
-              {lang.index.showcase.title}
+              {index.showcase.title}
             </h2>
             <p>Coming Soon ...</p>
           </Container>
@@ -54,7 +56,7 @@ export default function IndexPage() {
         <section id="site-preview">
           <Container type={ContainerType.SMALL}>
             <h2 className="text-5xl lg:text-8xl uppercase tracking-wide text-right text-gray-800 font-bold mt-0 mb-4">
-              {lang.index.teaser.title}
+              {index.teaser.title}
             </h2>
             <p>Coming Soon...</p>
           </Container>
